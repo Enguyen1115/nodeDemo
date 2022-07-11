08a12bb7e1a63a58aef744c24872770fd4e9b7b7
Enguyen1115 <105744661+Enguyen1115@users.noreply.github.com>
2022-07-11 13:17:49 -0600

    update

--- example.js
+++ example.js
@@ -3,12 +3,12 @@ var todaysDate = require('./myCPRG352Date')
 var url = require('url')
 
 http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type' : 'text/html'});
-    res.write('Today\'s date is ' + todaysDate.myDateTime());
+    // res.writeHead(200, {'Content-Type':'text/html'});
+    // res.write('Today\'s date is ' + todaysDate.myDateTime());
     var d = url.parse(req.url, true).query;
-    var txt = d.year + " " + d.month
+    var txt = d.year + " " + d.month;
 
     // res.write(d.year + " " + d.month)
     res.end(txt);
-    res.end('Hello Nodejs!');
+    // res.end('Hello Nodejs!');
 }).listen(8080);
