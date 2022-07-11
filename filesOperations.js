73e4f0a2d78802f170656f0bb1c651c08b9df1cd
Enguyen1115 <105744661+Enguyen1115@users.noreply.github.com>
2022-07-11 13:32:53 -0600

    Add create a file function

--- filesOperations.js
+++ filesOperations.js
@@ -1,10 +1,19 @@
 var fs = require('fs')
 var http = require('http')
 
-http.createServer(function (req, res) {
-    fs.readFile('myFile.html', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data)
-        return res.end()
-    });
-}).listen(8080);
+// Read from file
+// http.createServer(function (req, res) {
+//     fs.readFile('myFile.html', function (err, data) {
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.write(data)
+//         return res.end()
+//     });
+// }).listen(8080);
+
+/*Create a file
+fs.appendFile('myNewFile.txt', 'Hello NodeJS!', function(err){ */
+//Create an empty file
+fs.open('myNewFile.txt', 'w', function (err, file) {
+    if (err) throw err
+    console.log('Saved!');
+})
