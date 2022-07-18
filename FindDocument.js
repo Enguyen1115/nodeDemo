a967aa2cfc43e4b5201bf7d0a33ea25e9f78fd48
Enguyen1115 <105744661+Enguyen1115@users.noreply.github.com>
2022-07-18 12:43:12 -0600

    Add find anything within the db

--- FindDocument.js
+++ FindDocument.js
@@ -5,9 +5,17 @@ MongoClient.connect(url, function(err, db){
     if(err) throw err;
     var dbo = db.db("mydb");
     
-    dbo.collection("customers").findOne({}, function(err, result){
+    //Find the first set of info on the array
+    // dbo.collection("customers").findOne({}, function(err, result){
+    //     if(err) throw err;
+    //     console.log(result.name)
+    //     db.close();
+    // })
+
+    //Find everything/anything in the array on the db
+    dbo.collection("customers").find({name: "ConnectMe Ltd"}).toArray(function(err, result){
         if(err) throw err;
-        console.log(result.name)
+        console.log(result[0].address)
         db.close();
     })
 });
