4b58b97d9cd160d119906e05697a721a34017b6a
Enguyen1115 <105744661+Enguyen1115@users.noreply.github.com>
2022-07-18 12:22:36 -0600

    Add in an id tag for the customer

--- InsertCustomer.js
+++ InsertCustomer.js
@@ -5,7 +5,7 @@ MongoClient.connect(url, function(err, db){
     if(err) throw err;
     var dbo = db.db("mydb");
     
-    var myObj = {name: "ConnectMe Ltd", address: "17 ave SE"};
+    var myObj = {_id: 123, name: "ConnectMe Ltd", address: "17 ave SE"};
     dbo.collection("customers").insertOne(myObj, function(err, res){
         if(err) throw err;
         console.log("1 document inserted")
