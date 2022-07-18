4048e6b7e05f2a671ac1d21ac9e007745b082471
Enguyen1115 <105744661+Enguyen1115@users.noreply.github.com>
2022-07-18 12:24:13 -0600

    Update to console log the id

--- InsertCustomer.js
+++ InsertCustomer.js
@@ -5,10 +5,11 @@ MongoClient.connect(url, function(err, db){
     if(err) throw err;
     var dbo = db.db("mydb");
     
-    var myObj = {_id: 123, name: "ConnectMe Ltd", address: "17 ave SE"};
+    var myObj = {_id: 456, name: "ViewTech Ltd", address: "16 ave SE"};
     dbo.collection("customers").insertOne(myObj, function(err, res){
         if(err) throw err;
         console.log("1 document inserted")
+        console.log(res)
         db.close();
     })
 });
