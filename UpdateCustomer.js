b89785c746bbb5dd560cfce3c53d21994ca2461d
Enguyen1115 <105744661+Enguyen1115@users.noreply.github.com>
2022-07-19 15:47:26 -0600

    Add update many entry method

--- UpdateCustomer.js
+++ UpdateCustomer.js
@@ -4,11 +4,23 @@ var url = "mongodb://localhost:27017/"
 MongoClient.connect(url, function(err, db){
     if(err) throw err;
     var dbo = db.db("mydb");
-    var query = {address: 'Lowstreet 4'}
-    var newAddress = {$set: {address: 'Lowstreet 5'}}
-    dbo.collection("customers").updateOne(query, newAddress, function(err, result){
+
+     //Update 1 entry
+    // var query = {address: 'Lowstreet 5'}
+    // var newAddress = {$set: {address: 'Lowstreet 6', name: 'Ethan'}}
+    // dbo.collection("customers").updateOne(query, newAddress, function(err, result){
+    //     if(err) throw err;
+    //     console.log('1 document updated')
+    //     db.close();
+    // })
+
+    //Update many entry
+    var query = {address: /^A/}
+    var newAddress = {$set: {address: 'Wattson st 6', name: 'Amelia'}}
+    dbo.collection("customers").updateMany(query, newAddress, function(err, result){
         if(err) throw err;
-        console.log('1 document updated')
+        console.log(result.matchedCount + ' document updated')
         db.close();
     })
+
 });
