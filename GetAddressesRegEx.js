7d0fccbe511a4670f13824b1378a39e88d6bfd33
Enguyen1115 <105744661+Enguyen1115@users.noreply.github.com>
2022-07-19 15:22:40 -0600

    Add a sort method

--- GetAddressesRegEx.js
+++ GetAddressesRegEx.js
@@ -4,8 +4,17 @@ var url = "mongodb://localhost:27017/"
 MongoClient.connect(url, function(err, db){
     if(err) throw err;
     var dbo = db.db("mydb");
-    var query = {address: /^1/, name: /^C/}
-    dbo.collection("customers").find(query).toArray(function(err, result){
+
+    //Find method
+    // var query = {address: /^1/, name: /^C/}
+    // dbo.collection("customers").find(query).toArray(function(err, result){
+    //     if(err) throw err;
+    //     console.log(result)
+    //     db.close();
+
+    //sorting method
+    var nameSort = {name: -1}
+    dbo.collection("customers").find({}).sort(nameSort).toArray(function(err, result){
         if(err) throw err;
         console.log(result)
         db.close();
